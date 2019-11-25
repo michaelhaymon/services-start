c9edf6ef9ed015185a66922c59c5e35ad0b9bbe1
michaelhaymon <mhaymon2010@gmail.com>
2019-11-25 10:51:21 -0600

    corrected AccountService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import { AppComponent } from "./app.component";
+import { AccountComponent } from "./account/account.component";
+import { NewAccountComponent } from "./new-account/new-account.component";
+import { LoggingService } from "./logging.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [LoggingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
