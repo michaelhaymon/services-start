fca1e04ea07b9b191a492cbabc40bc0dabcf0b87
michaelhaymon <mhaymon2010@gmail.com>
2019-11-25 12:20:20 -0600

    Made Service into actual service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from "./app.component";
 import { AccountComponent } from "./account/account.component";
 import { NewAccountComponent } from "./new-account/new-account.component";
 import { LoggingService } from "./logging.service";
+import { AccountsService } from "./accounts.service";
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [LoggingService],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
